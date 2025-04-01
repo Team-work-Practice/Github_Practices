c9ed8d2de7ea121c86ce13dc38fd57ddbe2f4d9c
AbdelrhamanKhaled <bedona213@gmail.com>
2025-04-01 17:41:59 +0200

    create B Screen

---
+++ app/src/main/java/com/example/github_practices/ScreenB.kt
@@ -0,0 +1,9 @@
+package com.example.github_practices
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenB(modifier: Modifier = Modifier) {
+    HelloWorldText()
+}
