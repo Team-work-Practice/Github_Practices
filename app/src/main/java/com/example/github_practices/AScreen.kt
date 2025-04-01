51c3a23917db8417164c3680e1ce9499f5c387b0
AbdelrhamanKhaled <bedona213@gmail.com>
2025-04-01 17:17:44 +0200

    create screen A

---
+++ app/src/main/java/com/example/github_practices/AScreen.kt
@@ -0,0 +1,12 @@
+package com.example.github_practices
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ScreenA(modifier: Modifier = Modifier) {
+    Column {
+        HelloWorldText()
+    }
+}
