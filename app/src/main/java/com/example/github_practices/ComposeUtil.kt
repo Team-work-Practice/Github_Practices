b00e5eef9adeee8f6d9b5827014d285f8972b043
Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>
2025-04-01 17:12:43 +0200

    -Added Hello Wolrd Text

---
+++ app/src/main/java/com/example/github_practices/ComposeUtil.kt
@@ -0,0 +1,10 @@
+package com.example.github_practices
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelloWorldText(modifier: Modifier = Modifier) {
+    Text(text = "Hello World" , modifier = modifier)
+}
