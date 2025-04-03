5112c4dd06a9cc9c6942a34e665591a9f96feace
Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>
2025-04-03 19:50:30 +0200

    - update screen a #4

--- app/src/main/java/com/example/github_practices/AScreen.kt
+++ app/src/main/java/com/example/github_practices/AScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 @Composable
 fun ScreenA(modifier: Modifier = Modifier) {
     Column {
+        print("Hello World")
         HelloWorldText()
         HelloWorldText()
         HelloWorldText()
