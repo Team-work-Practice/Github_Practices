9580edc2e8e9de8fd434ea881c172aec19b3cb74
Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>
2025-04-03 18:16:34 +0200

    -added new row in screen b

--- app/src/main/java/com/example/github_practices/ScreenB.kt
+++ app/src/main/java/com/example/github_practices/ScreenB.kt
@@ -1,9 +1,13 @@
 package com.example.github_practices
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ScreenB(modifier: Modifier = Modifier) {
     HelloWorldText()
+    Row {
+
+    }
 }
