94d59e85325c8d1f481d47883ccd4d5f10d048b8
Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>
2025-04-04 04:25:09 +0200

    - new edit #2 From A

--- app/src/main/java/com/example/github_practices/ComposeUtil.kt
+++ app/src/main/java/com/example/github_practices/ComposeUtil.kt
@@ -9,4 +9,5 @@ fun HelloWorldText(modifier: Modifier = Modifier) {
     Text(text = "Hello World!!" , modifier = modifier)
     Text(text = "Hello World!!!" , modifier = modifier)
     Text(text = "Edit #2 from A" , modifier = modifier)
+    Text(text = "new Edit #2 from A" , modifier = modifier)
 }
