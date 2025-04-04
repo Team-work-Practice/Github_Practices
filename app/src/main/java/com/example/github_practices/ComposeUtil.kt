e75b46a7164d5639b08cde5590ba8586daab2d02
AbdelrhamanKhaled <73081160+AbdelrhamanKhaled@users.noreply.github.com>
2025-04-04 05:15:06 +0200

    Branch b (#5)
    * - new edit #1 from B
    
    * - new edit #2 from B and A
    
    * - new edit #3 From B
    
    * - new edit #4 From B
    
    * - new edit #5 From B
    
    ---------
    
    Co-authored-by: Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>

--- app/src/main/java/com/example/github_practices/ComposeUtil.kt
+++ app/src/main/java/com/example/github_practices/ComposeUtil.kt
@@ -9,5 +9,10 @@ fun HelloWorldText(modifier: Modifier = Modifier) {
     Text(text = "Hello World!!" , modifier = modifier)
     Text(text = "Hello World!!!" , modifier = modifier)
     Text(text = "Edit #2 from A" , modifier = modifier)
+    Text(text = "new Edit #2 from B" , modifier = modifier)
     Text(text = "new Edit #2 from A" , modifier = modifier)
+    Text(text = "new Edit #2 from B" , modifier = modifier)
+    Text(text = "new Edit #3 from B" , modifier = modifier)
+    Text(text = "new Edit #3 from B" , modifier = modifier)
+    Text(text = "new Edit #3 from B" , modifier = modifier)
 }
