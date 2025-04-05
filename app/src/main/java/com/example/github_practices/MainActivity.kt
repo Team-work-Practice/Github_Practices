aeacff1a39b0df56fc490feb66e4fdc493f92397
Abdelrahman-SW <abdelrahman.khaled.sw@gmail.com>
2025-04-06 01:07:12 +0200

    - update main activity

--- app/src/main/java/com/example/github_practices/MainActivity.kt
+++ app/src/main/java/com/example/github_practices/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
           Text(text = "Hello world !")
+          Text(text = "Hello world !")
         }
     }
 }
